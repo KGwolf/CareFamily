5f30bd2ac80ac99a4db9fa7a199ee0cd964ae79c
xiahaitao <630395100@qq.com>
2025-12-21 17:44:35 +0800

    新版首页第一次

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,32 @@
 Page({
   data: {
-    statusBarHeight: 0
+    familyMembers: [
+      {
+        id: 1,
+        name: '妈妈',
+        age: 72,
+        avatar: '/images/mom-avatar.png',
+        reminders: [
+          { id: 1, time: '08:00', desc: '吃降压药', completed: false },
+          { id: 2, time: '21:00', desc: '测血压', completed: false }
+        ]
+      },
+      {
+        id: 2,
+        name: '爸爸',
+        age: 74,
+        avatar: '/images/dad-avatar.png',
+        reminders: [
+          { id: 3, time: '14:00', desc: '复查提醒', completed: true }
+        ]
+      }
+    ]
   },
-  onLoad() {
-    const { statusBarHeight } = wx.getWindowInfo()
-    this.setData({ statusBarHeight })
+
+  toggleCompleted(e) {
+    const { index, member } = e.currentTarget.dataset;
+    const newFamily = this.data.familyMembers;
+    newFamily[member].reminders[index].completed = !newFamily[member].reminders[index].completed;
+    this.setData({ familyMembers: newFamily });
   }
-})
+});
